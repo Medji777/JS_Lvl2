ea8f5785a30a70ebcdaccfee37eec90997ec13c8
Medji <17dergachev@gmail.com>
2018-12-27 13:12:19 +0300

    Форматирование строк кода

--- Homework_2/Request/request.js
+++ Homework_2/Request/request.js
@@ -1,32 +1,32 @@
 var btn1 = document.querySelector('#bt_1'),
-	btn2 = document.querySelector('#bt_2'),
-	div = document.createElement('div');
+    btn2 = document.querySelector('#bt_2'),
+    div = document.createElement('div');
 document.body.appendChild(div);
 btn1.addEventListener('click', request);
 btn2.addEventListener('click', request);
+
 function request() {
-	//var json = `./${this.id}.json`;
+    //var json = `./${this.id}.json`;
     var xhr = new XMLHttpRequest();
-	//console.log(json);
+    //console.log(json);
     xhr.open('GET', `./${this.id}.json`, false);
     xhr.send();
     if (xhr.status !== 200) {
         console.log('Error', xhr.status, xhr.statusText)
     } else {
-		//console.log('ok', xhr.responseText);
+        //console.log('ok', xhr.responseText);
         var items = JSON.parse(xhr.responseText);
-		console.log(items);
-		for(var key in items){
-			if (items[key].result == 'error') {
-				console.log(items[key].result);
-				div.innerText = 'massage: ' + items[key].result;
-			}
-			if (items[key].result == 'success') {
-				console.log(items[key].result);
-				div.innerText = 'massage: ' +  items[key].result;
-			}
-		}
-        
+        console.log(items);
+        for (var key in items) {
+            if (items[key].result == 'error') {
+                console.log(items[key].result);
+                div.innerText = 'massage: ' + items[key].result;
+            }
+            if (items[key].result == 'success') {
+                console.log(items[key].result);
+                div.innerText = 'massage: ' + items[key].result;
+            }
+        }
+
     }
 }
-
