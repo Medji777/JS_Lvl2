30e2b588871a70abd174456d2dd4d8c34186e3bc
Medji <17dergachev@gmail.com>
2019-01-16 15:06:01 +0300

    Autocomplite on pure Js

---
+++ autocomplite/main.js
@@ -0,0 +1,55 @@
+let xhr = new XMLHttpRequest();
+xhr.open('GET', 'db.json', true);
+xhr.onreadystatechange = function(){
+  if(xhr.readyState !== 4){
+      return;
+  }
+  if(xhr.status !== 200){
+      console.log('Error', xhr.status, xhr.statusText);
+  } else {
+      console.log('Ok', xhr.responseText);
+      let dbData = JSON.parse(xhr.responseText),
+		  input = document.querySelector('.form_select'),
+		  div = document.querySelector('.select_result'),
+		  s = [];
+	  //let ps = document.querySelector('.option_result');
+	  dbData.forEach(function(node){
+		  s.push(node.city);
+	  });
+	  
+	  input.addEventListener('input', function() {
+    	let reg = new RegExp('^' + this.value, 'i');
+    	div.innerHTML = '';
+    	div.style.visibility = "hidden";
+		  
+    	if (this.value.length > 2){
+    		for (let i = 0; i < s.length; i++) {
+    			if (s[i].match(reg)) {
+					let p = document.createElement('p');
+					//let ps = `<p>${s[i]}</p>`;
+					p.setAttribute('class', 'option_result');
+					div.appendChild(p);
+					p.innerHTML += s[i];
+    				div.style.visibility = "visible";
+        		}
+			}
+    	}
+	  }
+	);
+	  //let ps = document.querySelector('.option_result');
+	  div.addEventListener('click', function () {
+		let p = document.querySelectorAll('.select_result .option_result');
+		  p.forEach(function(i){
+			  input.value = i.innerText;
+		  });
+		//let p = document.querySelector('.select_result .option_result');
+		  //p.forEach(function(i){
+			  //input.value = this.innerText;
+		  //});
+    	
+    	div.innerHTML = '';
+    	div.style.visibility = "hidden";
+	  });
+  }
+};
+xhr.send();
